e6c3f80162daa6f106a02e9c69773084c6f2b2a3
Nithyadevi <nithi2218@gmail.com>
2022-03-07 10:48:29 +1100

    Assignment Completed

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+  return "This one is on me!";
+  }
+  if (feet > 2000 && feet <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  if (feet > 2500) {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city){
+
+  let result = (city == 'NYC') ? 'Ok, sounds good.': 'No go.';
+  return result;
+  
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip){
+
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      
+    case 'not as generous':
+      return 'Thank you.';
+      
+    default:
+      return 'Bye.';  
+  }
+
 }
